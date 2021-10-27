1c720be9b33eea563ef8e1842b47be399f073051
Andriy Paliychuk <wantstraddle@gmail.com>
2021-10-27 19:54:39 +0300

    happy learning

--- src/main/java/com/bobocode/config/DispatcherServletConfig.java
+++ src/main/java/com/bobocode/config/DispatcherServletConfig.java
@@ -1,21 +1,8 @@
 package com.bobocode.config;
 
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+/**
+ * configure DispatcherServletConfig
+ */
+public class DispatcherServletConfig {
 
-
-public class DispatcherServletConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{HelloMvcConfig.class};
-    }
-
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
-    }
-
-    @Override
-    protected String[] getServletMappings() {
-        return new String[]{"/"};
-    }
 }
--- src/main/java/com/bobocode/controller/MessageRestController.java
+++ src/main/java/com/bobocode/controller/MessageRestController.java
@@ -1,67 +1,65 @@
 package com.bobocode.controller;
 
 import com.bobocode.dto.Message;
-import com.bobocode.exception.MessageNotFoundException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 import java.util.Collection;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
-@RestController
-@RequestMapping("/messages")
+/**
+ * create rest controller with "/messages" endpoint
+ */
 public class MessageRestController {
-    private AtomicLong idGenerator = new AtomicLong();
-    private Map<Long, Message> messageMap = new ConcurrentHashMap<>();
 
-    @PostMapping("/message")
+    /**
+     * save and return Message
+     */
     public Message addMessage(@RequestBody Message message) {
         return addNewMessage(message);
     }
 
-    @PostMapping("/requestentity")
+
+    /**
+     * print X-Mood header and save message
+     * return saved message
+     * @return
+     */
     public Message addMessage(RequestEntity<Message> messageRequestEntity) {
-        Optional.ofNullable(messageRequestEntity
-                        .getHeaders()
-                        .get("X-Mood"))
-                .ifPresent(System.out::println);
-        return addNewMessage(Objects.requireNonNull(messageRequestEntity.getBody()));
+        return null;
     }
 
-    @PostMapping("/responseentity")
-    public ResponseEntity<Message> addMessageWithResponse(RequestEntity<Message> messageRequestEntity) {
-        Optional.ofNullable(messageRequestEntity
-                        .getHeaders()
-                        .get("X-Mood"))
-                .ifPresent(System.out::println);
-        Message message = addNewMessage(Objects.requireNonNull(messageRequestEntity.getBody()));
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .location(URI.create("http://93.175.204.87:8080/messages" + message.getId()))
-                .body(message);
+    /**
+     * print to console X-Mood header from RequestEntity, get Message from body and save it
+     * return ResponseEntity with status created, location of saved message and Message itself
+     * @return
+     */
+    public ResponseEntity<Message> addMessageWithResponse() {
+        return null;
     }
 
-    @GetMapping("/{id}")
-    public Message getOne(@PathVariable Long id) {
-        return Optional.ofNullable(messageMap.get(id))
-                .orElseThrow(() -> new MessageNotFoundException("Cannot find message by id = " + id));
+    /**
+     * get message by id
+     * @return
+     */
+    public Message getOne() {
+        return null;
     }
 
-    @GetMapping
+    /**
+     * this method get all messages from map as a collection
+     * @return
+     */
     public Collection<Message> getAll() {
-        return messageMap.values();
+        return null;
     }
 
+    /**
+     * this method set id to message and add it to map
+     * @param message
+     * @return message
+     */
     private Message addNewMessage(Message message) {
-        message.setId(idGenerator.incrementAndGet());
-        messageMap.put(message.getId(), message);
         return message;
     }
 }
