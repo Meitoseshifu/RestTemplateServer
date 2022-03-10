26ea81aac91bd432f560bb379a338322090c8679
Andriy Paliychuk <wantstraddle@gmail.com>
2022-03-10 16:53:21 +0200

    mend

--- src/main/java/com/bobocode/config/HelloMvcConfig.java
+++ src/main/java/com/bobocode/config/HelloMvcConfig.java
@@ -1,7 +1,9 @@
 package com.bobocode.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,5 +11,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @ComponentScan("com.bobocode")
 public class HelloMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    @Scope
+    public Object getObject() {
+        return new Object();
+    }
 
 }
